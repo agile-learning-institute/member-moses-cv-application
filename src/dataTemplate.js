dddf66d36590daf5ef2b172fff40b3c524bdf89f
wodpachua <molarag21996@gmail.com>
2024-09-30 12:55:07 +0300

    Update template and script command

--- src/dataTemplate.js
+++ src/dataTemplate.js
@@ -11,11 +11,11 @@ const dataTemplate = {
   sections: {
     educations: [
       {
-        degree: "Bachelors in Land Surveying",
-        schoolName: "Makerere University",
-        location: "Kampala, Uganda",
-        startDate: "08/2015",
-        endDate: "12/2019",
+        degree: "Agile Software Engineering",
+        schoolName: "Agile Learning Institute",
+        location: "Remote",
+        startDate: "02/2024",
+        endDate: "Present",
         isCollapsed: true,
         isHidden: false,
         id: uuid(),
@@ -38,7 +38,7 @@ const dataTemplate = {
         positionTitle: "Volunteer Software Developer",
         location: "USA",
         description:
-          "Designed and prototyped user interface patterns for various clients in various industries, ranging from self-service apps within the telecommunications-sector to mobile games for IOS and Android",
+          "Code contributions | Involved in Global Events planning | OpenMRS Academy",
         startDate: "06/2023",
         endDate: "present",
         isCollapsed: true,
@@ -49,7 +49,7 @@ const dataTemplate = {
         positionTitle: "Land Surveyor",
         location: "Kampala, Uganda",
         description:
-          "Supported senior researchers on accessibility standards for the open web. Created and usability tested wireframes and prototypes. Produced interactive documentation for quick onboarding of new researchers.",
+          "Land surveys | Data processing | Topographical Mapping | GIS mapping | Title Processing | Boundary Openings | Subdivision Surveys",
         startDate: "04/2016",
         endDate: "02/2019",
         isCollapsed: true,
